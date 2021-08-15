12e8feb849ccd4682c1e7b2ae7c229eb4ac81b60
tatquangkiet789 <tatquangkiet@gmail.com>
2021-08-15 19:19:05 +0700

    Them 2 components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import ChatRoom from './components/ChatRoom/ChatRoom';
+import SignIn from './components/SignIn/SignIn';
 
 function App() {
   return (
     <div className="App">
       <h1>App Component</h1>
+      <SignIn />
+      <ChatRoom />
     </div>
   );
 }
---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignIn: React.FC = () => {
+    return (
+        <div>
+            <h1>This is SignIn Component</h1>
+        </div>
+    )
+}
+
+export default SignIn;
