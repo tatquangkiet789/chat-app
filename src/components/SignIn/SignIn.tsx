5e7465ad7d6c81e75d719c192723b31d9ff2df41
tatquangkiet789 <tatquangkiet@gmail.com>
2021-08-30 11:48:19 +0700

    Them giao dien cho SignIn component

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import firebase from 'firebase' 
 import { auth, db } from '../../firebase'
+import style from './SignIn.module.css'
 
 const SignIn: React.FC = () => {
     const signInWithGoogle = async () => {
@@ -21,8 +22,14 @@ const SignIn: React.FC = () => {
     }
 
     return (
-        <div>
-            <button onClick={signInWithGoogle}>Sign in with Google</button>
+        <div className={style.container}>
+            <div className={style.holder}>
+                <div className={style.image}></div>
+                <div className={style.buttonContainer}>
+                    <h2 className={style.text}>Welcome to DoanChat</h2>
+                    <button className={style.signInButton} onClick={signInWithGoogle}>Sign in with Google</button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Spin/Spin.tsx
+++ src/components/Spin/Spin.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import style from './Spin.module.css'
 
 const Spin: React.FC = () => {
-    return <div className={style.spin}></div>
+    return (
+        <div className={style.container}>
+            <div className={style.spin}></div>
+        </div>
+    );
 }
 
 export default Spin;
